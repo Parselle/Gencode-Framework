601cf1d6349ee123c769779318a413e2021478ee
Parselle <parselledev@gmail.com>
2018-07-16 00:41:56 +0200

    Version 1.0.3
    1. Update gulpfile.js
    2. Update js-core

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 'use strict';
 
 //Add plugin
-let gulp = require('gulp'),
+const gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     browserSync = require('browser-sync'),
     watch = require('gulp-watch'),
@@ -22,17 +22,14 @@ let gulp = require('gulp'),
     csso = require('gulp-csso'),
     pxrem = require('gulp-px-to-rem'),
 
-    concat = require('gulp-concat'),
+    babel = require('gulp-babel'),
     uglify = require('gulp-uglifyes'),
 
     imagemin = require('gulp-imagemin'),
-    pngquant = require('gulp-pngquant'),
-
-    reload = browserSync.reload;
-	
+    pngquant = require('gulp-pngquant');
 
 //Variable Path
-let path = {
+const path = {
     
     //Way for complete(build) files
     build: {
@@ -48,7 +45,7 @@ let path = {
 	
 	//Way for src(dev) files
 	src: {
-        pug: 'src/pug-core/*.pug',
+        pug_core: 'src/pug-core/*.pug',
         js_core: 'src/js-core/main.js',
         sass_core: 'src/sass-core/main.sass',
         assets: {
@@ -58,18 +55,19 @@ let path = {
                 'src/assets/**/*.*',
                 '!src/assets/img/**/*.*',
                 '!src/assets/fonts/**/*.*'
-              ]
+            ]
         }
     },
 	
 	//Files to watch
     watch: {
-        pug: 'src/pug-core/**/*.pug',
+        pug_core: 'src/pug-core/**/*.pug',
         js_core: 'src/js-core/**/*.js',
         sass_core: 'src/sass-core/**/*.sass',
         assets: {
             img: 'src/assets/img/**/*.*',
-            fonts: 'src/assets/fonts/**/*.*'
+            fonts: 'src/assets/fonts/**/*.*',
+            other: 'src/assets/**/*.*'
         }
     },
     
@@ -77,44 +75,41 @@ let path = {
     clean: './build'
 };
 
-//Server config
-let config = {
-    server: {
-        baseDir: "./build"
-    },
-    tunnel: true,
-    host: 'localhost',
-    port: 3001,
-    logPrefix: "localhost"
-};
 
 //LiveReload
+const reload = browserSync.reload;
 gulp.task('webserver', function () {
-    browserSync(config);
+    browserSync({
+        server: {
+            baseDir: "./build"
+        },
+        tunnel: false,
+        host: 'localhost',
+        port: 9000,
+        logPrefix: "localhost"
+    });
 });
 
 //Merge Pug files (Dev-build)
 gulp.task('pug_core', function () {
-    return gulp.src(path.src.pug) //Choose files
+    return gulp.src(path.src.pug_core) //Choose files
         .pipe(plumber()) //Run error handler
         .pipe(rigger()) //Tunnel to rigger
         .pipe(htmlhint()) //Validate html
         .pipe(plumber.stop()) //Stop error handler
         .pipe(pug())
         .pipe(gulp.dest(path.build.pug_core)) //Past to build folder
-        .pipe(reload({stream: true})); //Restart server
 });
 
 //Merge Pug files (Prod-build)
 gulp.task('pug_core:prod', function () {
-    return gulp.src(path.src.pug) //Choose files
+    return gulp.src(path.src.pug_core) //Choose files
         .pipe(plumber()) //Run error handler
         .pipe(rigger()) //Tunnel to rigger
         .pipe(htmlhint()) //Validate html
         .pipe(plumber.stop()) //Stop error handler
         .pipe(pug())
         .pipe(gulp.dest(path.build.pug_core)) //Past to build folder
-        .pipe(reload({stream: true})); //Restart server
 });
 
 //Merge JS files (Dev-build)
@@ -123,7 +118,7 @@ gulp.task('js_core', function () {
         .pipe(plumber()) //Run error handler
         .pipe(rigger()) //Tunnel to rigger
         .pipe(plumber.stop()) //Stop error handler
-        .pipe(gulp.dest(path.build.js_core)); //Past to build folder
+        .pipe(gulp.dest(path.build.js_core)) //Past to build folder
 });
 
 //Merge JS files (Prod-build)
@@ -132,8 +127,10 @@ gulp.task('js_core:prod', function () {
         .pipe(plumber()) //Run error handler
         .pipe(rigger()) //Tunnel to rigger
         .pipe(sourcemaps.init()) //Initializing sourcemap
-        .pipe(uglify()) //Min ES6+
-        //.pipe(concat('app.js')) //Merge all files
+        .pipe(babel({ // Run compiler from ES6+ to ES5
+            presets: ['env']
+        }))
+        .pipe(uglify()) //Min ES5
         .pipe(sourcemaps.write()) //Write Maps
         .pipe(plumber.stop()) //Stop error handler
         .pipe(gulp.dest(path.build.js_core)); //Past to build folder
@@ -141,10 +138,6 @@ gulp.task('js_core:prod', function () {
 
 //Merge Sass files (Dev-build)
 gulp.task('sass_core', function () {
-	
-	let plugins = [  //Init postcss plugins
-	    mqpacker //Merge same @media
-	];
 	
     return gulp.src(path.src.sass_core) //Choose main.sass
         .pipe(plumber()) //Run error handler
@@ -154,7 +147,6 @@ gulp.task('sass_core', function () {
         .pipe(pxrem({rootPX: 16})) // Convert PX to REM
         .pipe(plumber.stop()) //Stop error handler
         .pipe(gulp.dest(path.build.sass_core)) //Past to build folder
-        .pipe(reload({stream: true}));  //Restart server
 });
 
 //Merge Sass files (Prod-build)
@@ -176,7 +168,6 @@ gulp.task('sass_core:prod', function () {
         .pipe(sourcemaps.write())  //Write Maps
         .pipe(plumber.stop()) //Stop error handler
         .pipe(gulp.dest(path.build.sass_core)) //Past to build folder
-        .pipe(reload({stream: true}));  //Restart server
 });
 
 //Copy images (Dev-build)
@@ -185,7 +176,6 @@ gulp.task('assets:img', function () {
         .pipe(plumber()) //Run error handler
         .pipe(plumber.stop()) //Stop error handler
         .pipe(gulp.dest(path.build.assets.img)) //Past to build folder
-        .pipe(reload({stream: true}));  //Restart server
 });
 
 //Copy images (Prod-build)
@@ -200,7 +190,6 @@ gulp.task('assets:img:prod', function () {
         }))
         .pipe(plumber.stop()) //Stop error handler
         .pipe(gulp.dest(path.build.assets.img)) //Past to build folder
-        .pipe(reload({stream: true}));  //Restart server
 });
 
 //Copy fonts (Dev-build)
@@ -231,29 +220,35 @@ gulp.task('assets:other:prod', function() {
 
 //Watch Tasks
 gulp.task('watch', function(){
-    watch([path.watch.pug_core], function(event, cb) {
-        gulp.start('pug_core');
-    });
+    gulp.watch([path.watch.pug_core], gulp.series(
+        'pug_core',
+        reload
+    ));
 
-    watch([path.watch.js_core], function(event, cb) {
-        gulp.start('js_core');
-    });
+    gulp.watch([path.watch.js_core], gulp.series(
+        'js_core',
+        reload
+    ));
 
-    watch([path.watch.sass_core], function(event, cb) {
-        gulp.start('sass_core');
-    });
+    gulp.watch([path.watch.sass_core], gulp.series(
+        'sass_core',
+        reload
+    ));
 
-    watch([path.watch.assets.img], function(event, cb) {
-        gulp.start('assets:img');
-    });
+    gulp.watch([path.watch.assets.img], gulp.series(
+        'assets:img',
+        reload
+    ));
 
-    watch([path.watch.assets.fonts], function(event, cb) {
-        gulp.start('assets:fonts');
-    });
+    gulp.watch([path.watch.assets.fonts], gulp.series(
+        'assets:fonts',
+        reload
+    ));
 
-    watch([path.watch.assets.other], function(event, cb) {
-        gulp.start('assets:other');
-    });
+    gulp.watch([path.watch.assets.other], gulp.parallel(
+        'assets:other',
+        reload
+    ));
 });
 
 //Clean build folder
@@ -285,5 +280,8 @@ gulp.task('build:prod', gulp.parallel(
 ));
 
 //Final Task
-gulp.task('dev', gulp.series('clean', 'build:dev', 'webserver', 'watch'));
+gulp.task('dev', gulp.series('clean', 'build:dev', gulp.parallel(
+    'webserver',
+    'watch'
+)));
 gulp.task('prod', gulp.series('clean', 'build:prod', 'webserver'));
